87120773447f6f19bed4d56673f2abff8580c1df
“Hojoun <hyoon20@ucsc.edu>
2025-08-02 23:48:05 +0800

    Initial commit

---
+++ models/basic_model.py
@@ -0,0 +1,29 @@
+from models.model import Model
+from tensorflow.keras import Sequential, layers
+from tensorflow.keras.layers.experimental.preprocessing import Rescaling
+from tensorflow.keras.optimizers import RMSprop, Adam
+
+class BasicModel(Model):
+    def _define_model(self, input_shape, categories_count):
+        # Your code goes here
+        # you have to initialize self.model to a keras model
+        self.model = Sequential([
+            layers.Rescaling(1./255, input_shape=input_shape),
+            layers.Conv2D(32, (3, 3), activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Conv2D(64, (3, 3), activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Flatten(),
+            layers.Dense(128, activation='relu'),
+            layers.Dropout(0.5),
+            layers.Dense(categories_count, activation='softmax')
+        ])
+    
+    def _compile_model(self):
+        # Your code goes here
+        # you have to compile the keras model, similar to the example in the writeup
+        self.model.compile(
+            optimizer=RMSprop(learning_rate=0.001),
+            loss='categorical_crossentropy',
+            metrics=['accuracy'],
+        )
